1d2b38309cdfd14eeca77576b911020d5f6f2a74
Adnan Niaz <adnanniaz77@yahoo.com>
2020-11-04 05:32:19 -0800

    applied styling to copytext alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,16 +14,38 @@ document.querySelector('.modal-backdrop').addEventListener('click', (e) => {
 
 // Show Floating Menu on Scroll
 document.addEventListener('scroll', (e) => {
-    if(pageYOffset >= document.querySelector('header').offsetHeight) {
-        document.querySelector('.hidden_menu').style.display = 'initial'
-        document.querySelector('.hidden_menu').style.transform = 'translateY(0)'
-    } else {
+    if(pageYOffset <= document.querySelector('header').offsetHeight - 100) {
         document.querySelector('.hidden_menu').style.transform = 'translateY(-200px)'
         document.querySelector('.hidden_menu').style.position = 'fixed'
         document.querySelector('.hidden_menu').style.top = '0'
         document.querySelector('.hidden_menu').style.left = '0'
+    } else {
+        document.querySelector('.hidden_menu').style.display = 'initial'
+        document.querySelector('.hidden_menu').style.transform = 'translateY(0)'
     }
 })
+
+// Gallery Filter
+function filterSelection(selector) {
+    const filterBtns = document.querySelectorAll('.project_nav_btn');
+
+    filterBtns.forEach(eachBtn => {
+        eachBtn.classList.remove('active')
+        if (eachBtn.dataset.name === selector) {
+            eachBtn.classList.add('active')
+        }
+    })
+
+    project.forEach(each => {
+       if (!each.classList.contains(selector)) {
+           each.classList.add('hide')
+           each.classList.remove('show')
+       } else {
+           each.classList.add('show')
+           each.classList.remove('hide')
+       }
+    })
+}
 
 // copy text to clipboard
 function copyText(target) {
@@ -35,8 +57,12 @@ function copyText(target) {
     alertText.style.opacity = '1'
     setTimeout(() => {
         alertText.style.opacity = '0'
+        document.querySelector('.hidden_menu').style.display = 'initial'
+        document.querySelector('.hidden_menu').style.transform = 'translateY(0)'
+        console.log('hi')
     }, 1500)
 }
+
 
 // copyToClipBoard function
 function copyToClipBoard (text) {
@@ -61,26 +87,4 @@ function copyToClipBoard (text) {
       document.body.removeChild(textarea);
     }
     }
-}
-
-// Gallery Filter
-function filterSelection(selector) {
-    const filterBtns = document.querySelectorAll('.project_nav_btn');
-
-    filterBtns.forEach(eachBtn => {
-        eachBtn.classList.remove('active')
-        if (eachBtn.dataset.name === selector) {
-            eachBtn.classList.add('active')
-        }
-    })
-
-    project.forEach(each => {
-       if (!each.classList.contains(selector)) {
-           each.classList.add('hide')
-           each.classList.remove('show')
-       } else {
-           each.classList.add('show')
-           each.classList.remove('hide')
-       }
-    })
 }
